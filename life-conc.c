e33923659952a16a04f80f4b4c665d4938c9e8f9
Nick Fireman <nfireman1@gmail.com>
2020-02-19 15:13:28 -0500

    add multiple iterations and early exit

--- life-conc.c
+++ life-conc.c
@@ -3,78 +3,74 @@
 #include <pthread.h>
 #include "life.h"
 
-#define WORKER_THREADS 8
+#define WORKER_THREADS 7
 
 pthread_mutex_t mutex = PTHREAD_MUTEX_INITIALIZER;
-pthread_cond_t cond = PTHREAD_COND_INITIALIZER;
+pthread_cond_t cond_iter = PTHREAD_COND_INITIALIZER;
+pthread_cond_t cond_done = PTHREAD_COND_INITIALIZER;
 
 pthread_t thread_pool[WORKER_THREADS + 1];
 int ids[WORKER_THREADS];
-int progress = 0;
-int iter = 0;
+int iter_progress = 0;
+int done = 0;
 
-void run_game(cell *W, int N, cell *temp) {
-  cell *in = W;
-  cell *out = temp;
-
-  for (int i = 0; i < MAX_ITERATIONS; i++) {
-    in = i % 2 ? temp : W;
-    out = i % 2 ? W : temp;
-
-    if (all_dead(in, N)) {
-      if (in == temp) clear_grid(W, N);
-    }
-
-    next_grid_state(in, N, out);
-  }
-
-  if (out == temp) {
-    copy_grid(temp, N, W);
-  }
-}
-
-void print_game(cell *W, int N, cell *temp) {
-  printf("initial state:\n");
-  print_grid(W, N);
-
-  run_game(W, N, temp);
-
-  printf("\nfinal state:\n");
-  print_grid(W, N);
-}
+cell *grid = NULL;
+cell *temp = NULL;
+int N;
 
 void *worker(void *arg) {
   int id = *((int *) arg);
-  int done;
-  printf("worker thread %d initialized\n", id);
+  cell *in, *out;
 
-  for (int i = 0; i < 5; i++) {
+  // compute the indices of the grid that this thread will update
+  int min_index = id * N * N / WORKER_THREADS;
+  int max_index = (id + 1) * N * N / WORKER_THREADS - 1;
+
+  for (int i = 0; i < MAX_ITERATIONS; i++) {
+    // compute the source and target grids
+    in = i % 2 ? temp : grid;
+    out = i % 2 ? grid : temp;
+
+    for (int idx = min_index; idx <= max_index; idx++) {
+      out[idx] = next_cell_state(in, N, idx);
+    }
+
     pthread_mutex_lock(&mutex);
 
-    progress += 1;
-    printf("id: %d, progress: %d, iter: %d\n", id, progress, iter);
+    iter_progress += 1;
+    // printf("id: %d, iter_progress: %d, iter: %d\n", id, iter_progress, i);
 
-    if (progress == WORKER_THREADS) {
-      printf("done in id %d\n", id);
-      progress = 0;
-      iter += 1;
-      pthread_cond_broadcast(&cond);
+    if (iter_progress == WORKER_THREADS) {
+      // printf("done in id %d\n", id);
+      iter_progress = 0;
+
+      if (all_dead(out, N)) {
+        if (in == temp) clear_grid(in, N);
+        done = 1;
+        pthread_cond_broadcast(&cond_done);
+      } else {
+        pthread_cond_broadcast(&cond_iter);
+      }
     } else {
-      pthread_cond_wait(&cond, &mutex);
+      pthread_cond_wait(&cond_iter, &mutex);
     }
 
     pthread_mutex_unlock(&mutex);
+  }
+
+  if (id == 0) {
+    done = 1;
+    pthread_cond_broadcast(&cond_done);
   }
 
   pthread_exit(NULL);
 }
 
 int main() {
-  // int N = 11;
-  // cell *W = malloc(N * N * sizeof(cell));
-  // cell *temp = malloc(N * N * sizeof(cell));
-  // border_grid(W, N);
-  // print_game(W, N, temp);
+  N = 17;
+  grid = malloc(N * N * sizeof(cell));
+  temp = malloc(N * N * sizeof(cell));
+  border_grid(grid, N);
 
   for (int i = 0; i < WORKER_THREADS; i++) {
     ids[i] = i;
@@ -82,6 +78,10 @@ int main() {
       printf("failure creating worker thread\n");
       exit(1);
     }
+  }
+
+  while (!done) {
+    pthread_cond_wait(&cond_done, &mutex);
   }
 
   pthread_exit(NULL);
