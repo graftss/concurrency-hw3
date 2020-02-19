4d06781d0936b00169961ba030b27df6c984fd18
Nick Fireman <nfireman1@gmail.com>
2020-02-19 13:56:37 -0500

    add thread initialization

--- life-conc.c
+++ life-conc.c
@@ -3,6 +3,14 @@
 #include <pthread.h>
 #include "life.h"
 
+#define WORKER_THREADS 8
+
+pthread_mutex_t mutex = PTHREAD_MUTEX_INITIALIZER;
+pthread_cond_t cond = PTHREAD_COND_INITIALIZER;
+
+pthread_t thread_pool[WORKER_THREADS + 1];
+int ids[WORKER_THREADS];
+
 void run_game(cell *W, int N, cell *temp) {
   cell *in = W;
   cell *out = temp;
@@ -33,12 +41,36 @@ void print_game(cell *W, int N, cell *temp) {
   print_grid(W, N);
 }
 
+void *worker(void *arg) {
+  int id = *((int *) arg);
+  printf("worker thread %d initialized\n", id);
+  pthread_exit(NULL);
+}
+
+void *master() {
+  printf("master thread initialized\n");
+  pthread_exit(NULL);
+}
+
 int main() {
   int N = 11;
   cell *W = malloc(N * N * sizeof(cell));
   cell *temp = malloc(N * N * sizeof(cell));
 
-  border_grid(W, N);
+  for (int i = 0; i < WORKER_THREADS; i++) {
+    ids[i] = i;
+    if (pthread_create(&thread_pool[i], NULL, worker, (void *)(&ids[i]))) {
+      printf("failure creating worker thread\n");
+      exit(1);
+    }
+  }
 
-  print_game(W, N, temp);
+  if (pthread_create(&thread_pool[WORKER_THREADS], NULL, master, NULL)) {
+    printf("failure creating master thread\n");
+    exit(1);
+  }
+
+  pthread_exit(NULL);
+  // border_grid(W, N);
+  // print_game(W, N, temp);
 }
